e8e9dc6545607bacef0d4c5fae4a809a8df40993
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 15:59:23 -0300

    Criando o huk para conexao com api backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
@@ -9,9 +9,19 @@ function App() {
   // criando um huk para pegar os estado do formulario
   const [btnCadastrar, setBtnCadastrar] = useState(true);
 
+  const [produtos, setProdutos] = useState({});
 
+  //UseEffect = utilizado quando nosso componente é montado
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, [])
+
+  // retorno
   return (
     <div className="App">
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
