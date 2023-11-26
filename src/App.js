34802a0c013beb3bdac6ac9b19e0e3c2bbdba1ec
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 15:49:42 -0300

    Visibilidade dos botoes com useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+
+  // criando um huk para pegar os estado do formulario
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
+
   return (
     <div className="App">
-      <Formulario />
+      <Formulario botao={btnCadastrar} />
       <Tabela />
     </div>
   );
