faafb272de01a6403c8fa072f714f0430770ed7c
ClaytonEduard <clayton_eduard@hotmail.com>
2023-11-26 16:30:55 -0300

    criando o obj produto e criando o UseState produto

--- src/App.js
+++ src/App.js
@@ -6,21 +6,30 @@ import Tabela from './Tabela';
 
 function App() {
 
-  // criando um huk para pegar os estado do formulario
+  // * objeto produto
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: ''
+  }
+
+  //* UseState criando um huk para pegar os estado do formulario
   const [btnCadastrar, setBtnCadastrar] = useState(true);
-
   const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto)
 
-  //UseEffect = utilizado quando nosso componente é montado
+
+  //* UseEffect = utilizado quando nosso componente é montado
   useEffect(() => {
     fetch("http://localhost:8080/listar")
       .then(retorno => retorno.json())
       .then(retorno_convertido => setProdutos(retorno_convertido));
   }, []);
 
-  // retorno
+  //* retorno
   return (
     <div>
+      <p>{JSON.stringify(objProduto)}</p>
       <Formulario botao={btnCadastrar} />
       <Tabela vetor={produtos} />
     </div>
